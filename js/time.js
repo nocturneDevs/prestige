3a3825ed287748fd6f98c345d0c7d27ad0f6db38
Sanjay Guruprasad <sanjay.guruprasad@gmail.com>
2016-04-07 03:15:46 -0400

    Add time component
    - Displays current time in hh:mm:ss format
    - Not invoked anywhere yet
    - Added moment, because duh!

--- js/time.js
+++ js/time.js
@@ -1,1 +1,49 @@
-console.log("time")
+console.log("time");
+
+var TimeDisplay = React.createClass({
+  displayName: "TimeDisplay",
+
+  getInitialState: function() {
+    return({
+      time: "00:00:00"
+    });
+  },
+
+  getDefaultProps: function() {
+    return({
+      width: "100%",
+      color: "white",
+      backgroundColor: "rgba(0,0,0,0.4)"
+    });
+  },
+
+  componentDidMount: function() {
+    var that = this;
+    var blah = function() {
+      var newTime = moment().format("hh:mm:ss")
+      that.setState({
+        time: newTime
+      });
+    };
+    setInterval(blah, 100);
+  },
+
+  render: function() {
+    var style = {
+      timeBlock: {
+        zIndex: "99",
+        padding: "10px",
+        backgroundColor: this.props.backgroundColor,
+        width: this.props.width,
+        color: this.props.color,
+        textAlign: "center"
+      }
+    };
+    return (
+      React.createElement("div", {style: style.timeBlock}, 
+        React.createElement("h1", null, " ", this.state.time, " ")
+      )
+    );
+  }
+
+});
