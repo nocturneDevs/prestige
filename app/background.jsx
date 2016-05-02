6b3513eaaee9f67cdd2e2b4c9bd77645b09038ea
Sanjay Guruprasad <sanjay.guruprasad@gmail.com>
2016-05-02 16:22:33 +0530

    Add a vignette effect and remove link backgrounds
    - Links now fade in smoother (opacity jump fixed)
    - Links don’t have a background, instead, the main image now has a
    vignette using box-shadow

--- app/background.jsx
+++ app/background.jsx
@@ -86,6 +86,7 @@ var Background = React.createClass({
         display: "flex",
         alignItems: "center",
         justifyContent: "center",
+        boxShadow: "inset 0px 0px 85px rgba(0,0,0,.5)"
       }
     };
     return (
--- app/link.jsx
+++ app/link.jsx
@@ -11,10 +11,8 @@ var Link = React.createClass({
   render: function() {
     var style = {
       textDecoration: "none",
-      color: "#ccc",
-      opacity: "0.8",
+      color: "rgba(200,200,200,0.8)",
       padding: "5px",
-      backgroundImage: "radial-gradient(closest-side at 50% 50% , rgba(0,0,0,0.2) 0%, rgba(0,0,0,0) 100%)",
       WebkitAnimation: "fadein 1s"
     }
     return (
